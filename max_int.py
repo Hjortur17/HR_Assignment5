939ae5d383142669d2c75ac7922178b8c0d27181
Hjörtur Freyr <hjorturfreyr@hjorturfreyr.com>
2020-09-03 09:50:18 +0000

    Algorithm designed

---
+++ max_int.py
@@ -0,0 +1,3 @@
+# Algorithm: While user input is > or = to 0 we should propmt the user with an input asking for a intenger.
+# If the user inserts a negative number, the program should stop and print out the highest number he typed in
+# durring the perriod the program was running.
